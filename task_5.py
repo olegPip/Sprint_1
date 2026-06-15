d89bc4c39a9960363895cc6c08cf7ab016bfad9f
cybulin.oleg <olegpipulin@mail.ru>
2026-06-15 19:22:19 +0300

    task_5.py

--- task_5.py
+++ task_5.py
@@ -1,25 +1,25 @@
 class TestCase:
 
     def __init__(self):
-        self.steps = {}
+        self.steps = {}        # Инициализируем пустой словарь для шагов и переменной для ожидаемого результата
         self.result = None
 
     def set_step(self, step_number, step_text):
-        self.steps[step_number] = step_text 
+        self.steps[step_number] = step_text       # Добавляем в словарь шаг тест-кейса. Принимает 2 параметра ключ-значение (step_number-step_text)
+
     
-    
-    def delete_step(self, step_number):
+    def delete_step(self, step_number):          # Удаляем шаг из step по ключу step_number
         if step_number in self.steps:
             del self.steps[step_number]
 
-    def set_result(self, result):
+    def set_result(self, result):                 # Устанавливаем ожидаемый результат
         self.result = result
 
     def get_test_case(self):
 
 
-        info = {
-
+        info = {                                  # Выводи информацию о составе тест-кейса
+ 
             'Шаги': {
                 1: 'Перейти на сайт',
                 2: 'Перейти в раздел Товары',
