bf49badcd6ee7d37d1a3cd6766abcfeff17051b3
cybulin.oleg <olegpipulin@mail.ru>
2026-06-15 12:54:12 +0300

    task_3.py

--- task_3.py
+++ task_3.py
@@ -1,4 +1,4 @@
-worild_champions = {
+world_champions = {
     2002: "Бразилия",
     2006: "Италия",
     2010: "Испания",
@@ -7,16 +7,16 @@ worild_champions = {
 }
 
 # Добавили 2022 Аргентину в словарь
-worild_champions[2022] = "Аргентина"
+world_champions[2022] = "Аргентина"
 
 # Вывели всех чемпионов в формате год-страна
-for year, champiom in worild_champions.items():
+for year, champiom in world_champions.items():
     print(f"{year} - {champiom}")
 
 country = "Италия"
 
 # Проверяем, наличие в словаре страны
-if country in worild_champions.values():
+if country in world_champions.values():
     print(f"{country} становилась чемпионом мира по футболу в 21 веке!")
 else:
     print(f"{country} не выигрывала чемпионат мира по футболу в 21 веке.") 
